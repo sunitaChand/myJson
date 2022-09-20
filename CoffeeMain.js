a35e756c0e6ad88369d76f23df2a0b1e88cf2f59
sunitaChand <82164121+sunitaChand@users.noreply.github.com>
2022-09-20 14:49:40 +0530

    Update CoffeeMain.js

--- CoffeeMain.js
+++ CoffeeMain.js
@@ -5,14 +5,10 @@ $(function() {
 	var $name= $('#name');
 	var $drink= $('#drink');
 	$.ajax({
-				url: 'https://api.jsonbin.io/v3/b/6328b6555c146d63caa10824',
+				url: 'orders.json',
 				type: 'GET',
 				dataType:'json',
 				timeout:2500,
-				xhrFields: {
-                             RequestHeader:"$2b$10$0aq.if2DWEuLDP6YiQfoLOYXOlgTxNkyx1rfD.M8q/GmQuzV9xzQW",
-                          } ,
-
 				complete:function(orders)
 				{
 					alert('hi');
